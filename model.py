95896ff691f23f3c5448945b81afd48e04ea09b3
Jacky Jiang <jacky.jiang@verdiag.com>
2022-01-16 21:45:55 -0800

    added math model and ability to segment

---
+++ model.py
@@ -0,0 +1,27 @@
+import math
+import matplotlib.pyplot as plt
+
+delta_t = 17
+
+# units are W, mm, s, K
+thermal_conductivity = 0.598/1000 # W/mm*K @ 20 degrees
+thermal_diffusivity  = 0.143 # mm^2/s @ 25 degrees
+signal_strength      = 38
+distance_from_heater = 5 # mm
+speed                = 0.83 # mm/s
+
+def funk(t, q, v):
+    k = thermal_conductivity
+    a = thermal_diffusivity
+    # q = signal_strength
+    x = distance_from_heater
+    return (q/(4*math.pi*k*t)*math.e**(-(x-v*t)**2/(4*a*t)))
+
+
+if (__name__ == "__main__"):
+    t_list = [delta_t*i/1000 for i in range(1,588)]
+    rslt = []
+    for t in t_list: 
+        rslt.append(funk(t, 1, 1))
+    plt.plot(t_list,rslt)
+    plt.show()
